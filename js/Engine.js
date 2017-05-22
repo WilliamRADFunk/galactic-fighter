95abbf2d23b562f3b22d7c023dc82501f6a24408
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-05-21 21:16:04 -0400

    Removes functionality from update loop into individual functions. Also, destroys asteroid that impacts ship.

--- js/Engine.js
+++ js/Engine.js
@@ -234,6 +234,216 @@ Engine.Scene = function()
 		}
 	};
 };
+Engine.asteroidMovementHandler = function()
+{
+	for(var i = 0, j = 0; i < spaceDebris.length - j; i++)
+	{
+		moveProjectiles(spaceDebris[i]);
+		if(spaceDebris[i].position.x < -50)
+		{
+			// Player loses points for missing asteroid
+			score.addPoints(-spaceDebris[i].points);
+			// Remove asteroid as it leaves screen
+			scene.remove(spaceDebris[i]);
+			spaceDebris.splice(i, 1);
+			j++;
+		}
+	}
+	if(spaceDebris.length < asteroidDensity && Math.random() > 0.98)
+	{
+		var asteroid = new Engine.Asteroid(
+			Engine.canvas.width + 50,
+			Math.floor(Math.random() * (Engine.canvas.height - 30)),
+			0
+		);
+		spaceDebris.push(asteroid);
+		scene.add(asteroid);
+	}
+}
+Engine.asteroidProjectileCollisionHandler = function()
+{
+	for(var i = 0, j = 0; i < spaceDebris.length - j; i++)
+	{
+		for(var k = 0, l = 0; k < playerProjectiles.length - l; k++)
+		{
+			var projectile = {x: playerProjectiles[k].position.x, y: playerProjectiles[k].position.y, width: playerProjectiles[k].radius, height: playerProjectiles[k].radius};
+			var asteroid = {x: spaceDebris[i].position.x, y: spaceDebris[i].position.y, width: spaceDebris[i].size, height: spaceDebris[i].size}
+
+			if (projectile.x < (asteroid.x + asteroid.width)
+				&& (projectile.x + projectile.width) > asteroid.x
+				&& projectile.y < (asteroid.y + asteroid.height)
+				&& (projectile.height + projectile.y) > asteroid.y
+			) {
+				var explosionOuter = new Engine.Orb(
+					spaceDebris[i].position.x,
+					spaceDebris[i].position.y,
+					-1,
+					(spaceDebris[i].size),
+					[239, 74, 37, 0.01],
+					[239, 74, 37, 0.01]
+				);
+				explosions.push(explosionOuter);
+				scene.add(explosionOuter);
+				var explosionInner = new Engine.Orb(
+					spaceDebris[i].position.x,
+					spaceDebris[i].position.y,
+					-1,
+					(spaceDebris[i].size / 2),
+					[170, 13, 3, 0.01],
+					[170, 13, 3, 0.01]
+				);
+				explosions.push(explosionInner);
+				scene.add(explosionInner);
+				// Remove projectile
+				scene.remove(playerProjectiles[k]);
+				playerProjectiles.splice(k, 1);
+				l++;
+				// Increase player's points
+				score.addPoints(spaceDebris[i].points);
+				// Remove asteroid
+				scene.remove(spaceDebris[i]);
+				spaceDebris.splice(i, 1);
+				j++;
+				break;
+			}
+		}
+	}
+}
+Engine.asteroidShipCollisionHandler = function()
+{
+	for(var i = 0, j = 0; i < spaceDebris.length - j; i++)
+	{
+		var ship = {x: player.position.x, y: player.position.y, width: player.size, height: player.size};
+		var asteroid = {x: spaceDebris[i].position.x, y: spaceDebris[i].position.y, width: spaceDebris[i].size, height: spaceDebris[i].size}
+
+		if ((ship.x + (ship.width * 0.1)) < (asteroid.x + asteroid.width)
+			&& (ship.x + ship.width - (ship.width * 0.1)) > asteroid.x
+			&& (ship.y + (ship.height * 0.1)) < (asteroid.y + asteroid.height)
+			&& (ship.height + ship.y - (ship.height * 0.1)) > asteroid.y
+		) {
+			var explosionOuter = new Engine.Orb(
+				player.position.x,
+				player.position.y,
+				-1,
+				(player.size),
+				[239, 74, 37, 0.01],
+				[239, 74, 37, 0.01]
+			);
+			explosions.push(explosionOuter);
+			scene.add(explosionOuter);
+			var explosionInner = new Engine.Orb(
+				player.position.x,
+				player.position.y,
+				-1,
+				(player.size / 2),
+				[170, 13, 3, 0.01],
+				[170, 13, 3, 0.01]
+			);
+			explosions.push(explosionInner);
+			scene.add(explosionInner);
+
+			player.destroy();
+
+			explosionOuter = new Engine.Orb(
+				spaceDebris[i].position.x,
+				spaceDebris[i].position.y,
+				-1,
+				(spaceDebris[i].size),
+				[239, 74, 37, 0.01],
+				[239, 74, 37, 0.01]
+			);
+			explosions.push(explosionOuter);
+			scene.add(explosionOuter);
+
+			explosionInner = new Engine.Orb(
+				spaceDebris[i].position.x,
+				spaceDebris[i].position.y,
+				-1,
+				(spaceDebris[i].size / 2),
+				[170, 13, 3, 0.01],
+				[170, 13, 3, 0.01]
+			);
+			explosions.push(explosionInner);
+			scene.add(explosionInner);
+
+			scene.remove(spaceDebris[i]);
+			spaceDebris.splice(i, 1);
+			j++;
+			break;
+		}
+	}
+}
+Engine.exhaustParticleHandler = function()
+{
+	for(var i = 0; i < engineParticles.length; i++)
+	{
+		moveProjectiles(engineParticles[i]);
+		if(engineParticles[i].colorA > 0)
+		{
+			engineParticles[i].fade(Math.random() * (0.08 - 0.01) + 0.01);
+		}
+		
+		if(engineParticles[i].colorA <= 0 && !player.isDestroyed)
+		{
+			engineParticles[i].move(
+				player.position.x,
+				player.position.y + (player.size / 2) - (7 * Math.random())
+			);
+			engineParticles[i].fade(-1);
+		}
+	}
+}
+Engine.explosionHandler = function()
+{
+	for(var i = 0, j = 0; i < explosions.length - j; i++)
+	{
+		explosions[i].move(explosions[i].position.x + explosions[i].speed, explosions[i].position.y);
+		if(explosions[i].colorA > 0 && explosions[i].colorA <= 0.7 && explosions[i].isBlooming !== false)
+		{
+			explosions[i].fade(-0.02);
+			explosions[i].isBlooming = true;
+		}
+		else if(explosions[i].colorA > 0 || explosions[i].colorA > 0.7)
+		{
+			explosions[i].isBlooming = false;
+			explosions[i].fade(0.01);
+		}
+		else
+		{
+			// Remove explosion as it fades away
+			scene.remove(explosions[i]);
+			explosions.splice(i, 1);
+			j++;
+		}
+	}
+}
+Engine.projectileMovementHandler = function()
+{
+	for(var i = 0, j = 0; i < playerProjectiles.length - j; i++)
+	{
+		moveProjectiles(playerProjectiles[i]);
+		if(playerProjectiles[i].position.x >= Engine.canvas.width + 5)
+		{
+			scene.remove(playerProjectiles[i]);
+			playerProjectiles.splice(i, 1);
+			j++;
+		}
+	}
+}
+Engine.starMovementHandler = function()
+{
+	for(var i = 0; i < stars.length; i++)
+	{
+		moveProjectiles(stars[i]);
+		if(stars[i].position.x <= -5)
+		{
+			stars[i].move(
+				Engine.canvas.width + 50,
+				Math.floor(Math.random() * Engine.canvas.height)
+			);
+		}
+	}
+}
 // Engine's update cycle
 Engine.update = function(timestamp)
 {
@@ -248,180 +458,23 @@ Engine.update = function(timestamp)
 			movePlayer();
 		}
 		// Move the stars a their speeds.
-		for(var i = 0; i < stars.length; i++)
-		{
-			moveProjectiles(stars[i]);
-			if(stars[i].position.x <= -5)
-			{
-				stars[i].move(
-					Engine.canvas.width + 50, 
-					Math.floor(Math.random() * Engine.canvas.height)
-				);
-			}
-		}
+		Engine.starMovementHandler();
 		// Move, and remove player projectiles as they leave the screen.
-		for(var i = 0, j = 0; i < playerProjectiles.length - j; i++)
-		{
-			moveProjectiles(playerProjectiles[i]);
-			if(playerProjectiles[i].position.x >= Engine.canvas.width + 5)
-			{
-				scene.remove(playerProjectiles[i]);
-				playerProjectiles.splice(i, 1);
-				j++;
-			}
-		}
-		// Move, remove, and create player engine exhaust particles.
-		for(var i = 0; i < engineParticles.length; i++)
-		{
-			moveProjectiles(engineParticles[i]);
-			if(engineParticles[i].colorA > 0)
-			{
-				engineParticles[i].fade(Math.random() * (0.08 - 0.01) + 0.01);
-			}
-			
-			if(engineParticles[i].colorA <= 0 && !player.isDestroyed)
-			{
-				engineParticles[i].move(
-					player.position.x,
-					player.position.y + (player.size / 2) - (7 * Math.random())
-				);
-				engineParticles[i].fade(-1);
-			}
-		}
-		// Move, remove, and create (at random) asteroids as they leave the screen.
-		for(var i = 0, j = 0; i < spaceDebris.length - j; i++)
-		{
-			moveProjectiles(spaceDebris[i]);
-			if(spaceDebris[i].position.x < -50)
-			{
-				// Player loses points for missing asteroid
-				score.addPoints(-spaceDebris[i].points);
-				// Remove asteroid as it leaves screen
-				scene.remove(spaceDebris[i]);
-				spaceDebris.splice(i, 1);
-				j++;
-			}
-		}
-		if(spaceDebris.length < asteroidDensity && Math.random() > 0.98)
-		{
-			var asteroid = new Engine.Asteroid(
-				Engine.canvas.width + 50,
-				Math.floor(Math.random() * (Engine.canvas.height - 30)),
-				0
-			);
-			spaceDebris.push(asteroid);
-			scene.add(asteroid);
-		}
-		// Checks to see if ship was struck by an asteroid
+		Engine.projectileMovementHandler();
+		
 		if(!player.isDestroyed)
 		{
-			for(var i = 0, j = 0; i < spaceDebris.length - j; i++)
-			{
-				var ship = {x: player.position.x, y: player.position.y, width: player.size, height: player.size};
-				var asteroid = {x: spaceDebris[i].position.x, y: spaceDebris[i].position.y, width: spaceDebris[i].size, height: spaceDebris[i].size}
-
-				if ((ship.x + (ship.width * 0.1)) < (asteroid.x + asteroid.width)
-					&& (ship.x + ship.width - (ship.width * 0.1)) > asteroid.x
-					&& (ship.y + (ship.height * 0.1)) < (asteroid.y + asteroid.height)
-					&& (ship.height + ship.y - (ship.height * 0.1)) > asteroid.y
-				) {
-					var explosionOuter = new Engine.Orb(
-						player.position.x,
-						player.position.y,
-						-1,
-						(player.size),
-						[239, 74, 37, 0.01],
-						[239, 74, 37, 0.01]
-					);
-					explosions.push(explosionOuter);
-					scene.add(explosionOuter);
-					var explosionInner = new Engine.Orb(
-						player.position.x,
-						player.position.y,
-						-1,
-						(player.size / 2),
-						[170, 13, 3, 0.01],
-						[170, 13, 3, 0.01]
-					);
-					explosions.push(explosionInner);
-					scene.add(explosionInner);
-
-					player.destroy();
-				}
-			}
+			// Checks to see if ship was struck by an asteroid
+			Engine.asteroidShipCollisionHandler();
+			// Checks to see if asteroid was struck by a projectile
+			Engine.asteroidProjectileCollisionHandler();
 		}
 		// Handles movement and fading of explosions
-		for(var i = 0, j = 0; i < explosions.length - j; i++)
-		{
-			explosions[i].move(explosions[i].position.x + explosions[i].speed, explosions[i].position.y);
-			if(explosions[i].colorA > 0 && explosions[i].colorA <= 0.7 && explosions[i].isBlooming !== false)
-			{
-				explosions[i].fade(-0.02);
-				explosions[i].isBlooming = true;
-			}
-			else if(explosions[i].colorA > 0 || explosions[i].colorA > 0.7)
-			{
-				explosions[i].isBlooming = false;
-				explosions[i].fade(0.01);
-			}
-			else
-			{
-				// Remove explosion as it fades away
-				scene.remove(explosions[i]);
-				explosions.splice(i, 1);
-				j++;
-			}
-		}
-		// Checks to see if asteroid was struck by a projectile
-		if(!player.isDestroyed)
-		{
-			for(var i = 0, j = 0; i < spaceDebris.length - j; i++)
-			{
-				for(var k = 0, l = 0; k < playerProjectiles.length - l; k++)
-				{
-					var projectile = {x: playerProjectiles[k].position.x, y: playerProjectiles[k].position.y, width: playerProjectiles[k].radius, height: playerProjectiles[k].radius};
-					var asteroid = {x: spaceDebris[i].position.x, y: spaceDebris[i].position.y, width: spaceDebris[i].size, height: spaceDebris[i].size}
-
-					if (projectile.x < (asteroid.x + asteroid.width)
-						&& (projectile.x + projectile.width) > asteroid.x
-						&& projectile.y < (asteroid.y + asteroid.height)
-						&& (projectile.height + projectile.y) > asteroid.y
-					) {
-						var explosionOuter = new Engine.Orb(
-							spaceDebris[i].position.x,
-							spaceDebris[i].position.y,
-							-1,
-							(spaceDebris[i].size),
-							[239, 74, 37, 0.01],
-							[239, 74, 37, 0.01]
-						);
-						explosions.push(explosionOuter);
-						scene.add(explosionOuter);
-						var explosionInner = new Engine.Orb(
-							spaceDebris[i].position.x,
-							spaceDebris[i].position.y,
-							-1,
-							(spaceDebris[i].size / 2),
-							[170, 13, 3, 0.01],
-							[170, 13, 3, 0.01]
-						);
-						explosions.push(explosionInner);
-						scene.add(explosionInner);
-						// Remove projectile
-						scene.remove(playerProjectiles[k]);
-						playerProjectiles.splice(k, 1);
-						l++;
-						// Increase player's points
-						score.addPoints(spaceDebris[i].points);
-						// Remove asteroid
-						scene.remove(spaceDebris[i]);
-						spaceDebris.splice(i, 1);
-						j++;
-						break;
-					}
-				}
-			}
-		}
+		Engine.explosionHandler();
+		// Move, remove, and create (at random) asteroids as they leave the screen.
+		Engine.asteroidMovementHandler();
+		// Move, remove, and create player engine exhaust particles.
+		Engine.exhaustParticleHandler();
 		
 		scene.render();
 		start = null;
